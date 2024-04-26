f9eefba6ff1e29268ced9ad6e45f29e8f80a79b9
Thanh17082001 <thienthanh17082001@gmail.com>
2024-04-26 15:44:09 +0700

    test

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,17 @@
-import { Controller,Get,Body, Res } from '@nestjs/common';
+import { Controller,Get,Body, Res, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private userService:UsersService){}
-    @Get()
+    @Post()
     async create(@Body() data: object, @Res() res) {
-        res.json({mes:'thanh'})
+        try {
+            await this.userService.create(data);
+            res.status(200).json({ mes: 'create successfully' });
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
 
 }
